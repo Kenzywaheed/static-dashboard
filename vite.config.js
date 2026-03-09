812e8f3110f2c5a32ed510e78a75d2d5be3c8079
Kenzywaheed <kenzywaheed6@gmailcom>
2026-03-09 15:36:17 +0200

    Prepare project for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react()],
+  build: { outDir: 'dist' }
 })
 
